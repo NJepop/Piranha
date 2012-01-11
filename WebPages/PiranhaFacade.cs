fa1a4d8fd04783679ee13490548f003de886f4af
Håkan Edling <hakan@tidyui.com>
2012-01-11 21:22:50 +0100

    Added formats to the editor and some more post methods to the facade.

--- WebPages/PiranhaFacade.cs
+++ WebPages/PiranhaFacade.cs
@@ -17,9 +17,31 @@ namespace Piranha.WebPages
 		/// Gets the available posts for the given post type.
 		/// </summary>
 		/// <param name="name">The post type name.</param>
-		/// <returns>The posts</returns>
+		/// <returns>A list of posts</returns>
 		public List<Post> GetPostsByType(string name) {
 			return Post.Get("posttemplate_name LIKE @0", "%," + name, new Params() { OrderBy = "post_created DESC" }) ;
+		}
+
+		/// <summary>
+		/// Gets the posts for the given category name.
+		/// </summary>
+		/// <param name="name">The category name</param>
+		/// <returns>A list of posts</returns>
+		public List<Post> GetPostByCategoryName(string name) {
+			return Post.Get("post_id IN (" +
+				"SELECT relation_data_id FROM relation WHERE relation_type = @0 AND relation_related_id = (" +
+				"SELECT category_id FROM category WHERE category_name = @1))", Relation.RelationType.POSTCATEGORY, name) ;
+		}
+
+		/// <summary>
+		/// Gets the posts for the given category id.
+		/// </summary>
+		/// <param name="id">The category id</param>
+		/// <returns>A list of posts</returns>
+		public List<Post> GetPostByCategoryId(Guid id) {
+			return Post.Get("post_id IN (" +
+				"SELECT relation_data_id FROM relation WHERE relation_type = @0 AND relation_related_id = @1)",
+				Relation.RelationType.POSTCATEGORY, id) ;
 		}
 	}
 }
